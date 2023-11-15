f1d60099985d8eacd14c0f7a8a2bad8cfa74cbbc
anz-ryu <ryu@anchorz.co.jp>
2023-11-15 11:59:27 +0900

    init

---
+++ SiriCapability/SiriCapability/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SiriCapability
+//
+//  Created by anz-ryu on 2023/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
